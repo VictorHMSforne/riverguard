9fb2475341a87bf3b07cc31e7d77874e9ab03927
VictorHMSforne <vmendessforne@gmail.com>
2026-06-10 15:07:28 -0300

    Atualização 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/riverguard/'   // ← adicione esta linha
+  base: '/riverguard'   // ← adicione esta linha
 })
